652af752305af0ae8dcb3525d6dcbb6be42ec9e5
amaksix <amaksix@gmail.com>
2021-04-22 14:05:48 +0200

    Added Debugs to see what pirates are earning point from.  Changed Utility calculation for Pirates, and made scale to be depended on weight.

--- Assets/Scripts/PirateLogic.cs
+++ Assets/Scripts/PirateLogic.cs
@@ -8,17 +8,18 @@ public class PirateLogic : AgentLogic
     private static float _boxPoints = 0.1f;
     private static float _boatPoints = 5.0f;
     private static float _enemyPoints = 7.0f;
-    private static float minPirateWeight = 1;
-    private static float maxPirateWeight = 100;
+    private static float minPirateWeight = 50;
+    private static float maxPirateWeight = 150;
     private static float minPirateScaleMultiplier = 0.5f;
     private static float maxPirateScaleMultiplier = 1.5f;
     #endregion
-    private float weight;
-    private float scaleMultiplier;
+    public float Weight;
+    public float SceleMultiplier;
     private void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag.Equals("Box"))
         {
+            Debug.Log("BoxTaken");
             points += _boxPoints;
             Destroy(other.gameObject);
         }
@@ -26,13 +27,28 @@ public class PirateLogic : AgentLogic
     
     public override void AditionalInitialization()
     {
-        weight = Random.Range(minPirateWeight, maxPirateWeight);
-        scaleMultiplier = Random.Range(minPirateScaleMultiplier, maxPirateScaleMultiplier);
-        gameObject.transform.localScale = gameObject.transform.localScale * scaleMultiplier;
+        Weight = Random.Range(minPirateWeight, maxPirateWeight);
+        SceleMultiplier =Weight/100;
+        gameObject.transform.localScale = gameObject.transform.localScale * SceleMultiplier;
     }
 
     public override float RecalculateEnemyFactors(float distanceIndex, float enemyDistanceFactor, GameObject collidedObj)
     {
+        if(Weight>collidedObj.GetComponent<PirateLogic>().Weight)
+        {
+            return distanceIndex * enemyDistanceFactor + GetEnemyWeight();
+        }
+        else
+        {
+            if (GetEnemyWeight()>0)
+            {
+                return distanceIndex * enemyDistanceFactor - GetEnemyWeight();
+            }
+            else
+            {
+                return distanceIndex * enemyDistanceFactor + GetEnemyWeight();
+            }
+        }
         return distanceIndex * enemyDistanceFactor + GetEnemyWeight();
     }
 
@@ -41,16 +57,18 @@ public class PirateLogic : AgentLogic
         if(other.gameObject.tag.Equals("Boat"))
         {
             points += _boatPoints;
+            Debug.Log("Killed boat");
             Destroy(other.gameObject);
         }
         if (other.gameObject.tag.Equals("Enemy"))
         {
-            if (other.gameObject.GetComponent<PirateLogic>().GetEnemyWeight()> gameObject.GetComponent<PirateLogic>().GetEnemyWeight())
+            if (other.gameObject.GetComponent<PirateLogic>().Weight> gameObject.GetComponent<PirateLogic>().Weight)
             {
                 points -= _enemyPoints;
             }
             else
             {
+                Debug.Log("Killed pirate");
                 points += _enemyPoints;
                 Destroy(other.gameObject);
             }
